c91dc58f5fc75f3d2fd365feb6e9ab3e63141f4d
Martyna Chruściel <martynac@qualtrics.com>
2021-05-23 12:45:23 +0200

    Setup typescript and eslint

---
+++ src/main/ui/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const App = (): JSX.Element =>
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <p>
+        Hello world! Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello
+        world!Hello world!Hello world!Hello world!Hello world!
+      </p>
+    </header>
+  </div>
+
+
+export default App
