9b7f8fe8e980eba9b60c6d67d69883b9dbc84765
Martyna Chruściel <martynac@qualtrics.com>
2021-05-23 19:18:06 +0200

    Use /hello endpoint for displaying message in the UI

---
+++ src/main/java/com/example/KitchenProject/config/Config.java
@@ -0,0 +1,8 @@
+package com.example.KitchenProject.config;
+
+public class Config {
+    private Config() {
+    }
+
+    public static final String UI_ORIGIN = "http://localhost:3000/";
+}
--- src/main/ui/src/App.tsx
+++ src/main/ui/src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { NetworkService } from "./networkService";
 
-const App = (): JSX.Element =>
-  <div className="App">
+const App = (): JSX.Element => {
+
+  const [hello, setHello] = useState<string>("")
+
+  useEffect(() => {
+    getHello()
+  }, [])
+
+  const getHello = async (): Promise<void> => {
+    const service = new NetworkService()
+    const helloResponse = await service.getHello();
+    setHello(helloResponse.data)
+  }
+
+  return <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo"/>
       <p>
-        Hello world!
+        {hello}
       </p>
     </header>
   </div>
-
+}
 
 export default App
